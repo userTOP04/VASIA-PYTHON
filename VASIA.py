ac55f3d6e936062802a9bf9bdd3f84d42acaca35
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2023-03-28 17:40:11 +0300

    Add files via upload

--- VASIA.py
+++ VASIA.py
@@ -1,28 +1,50 @@
-class CyberHero:
-    def __init__(self, name, force, hp, dodge, luck, xp, inventory):
-        print("Кибер баготырь", name, "создан")
-        self.name = name
-        self.force = force
-        self.hp = hp
-        self.dodge = dodge
-        self.luck = luck
-        self.xp = xp
-        self.inventory = inventory
+class Ciberhero:
+    """docstring for ClassName"""
+    def __init__(self):
+        self.name = "Кибербогатырь"
+        self.hp = 100
+        self.at = 5
+        self.inv = []
+        self.dodge = 10
 
 
-hero1 = CyberHero(name="VASIA",force=10,hp=100,dodge=10,luck=1,xp=0,inventory=[])
-hero2 = CyberHero(name="VASIA",force=10,hp=100,dodge=10,luck=1,xp=0,inventory=[])
 
-def combat_turn(name, force, hp):
-    if hp > 0:
-        hp -= force
-        print(hero1.name, "ударил", hero2.name, "на", hero1.force, "жизней!")
 
-def start_fight(name, force, hp) -> None:
-    while hero1.hp > 0 and hero2.hp > 0:
-            combat_turn(hero1, hero2)
-            combat_turn(hero2, hero1)
-            print("")
-            print(hero1.hp)
-            print(hero2.hp)
+class CiberMag(Ciberhero):
+    def __init__(self):
+        self.name = "КиберМаг"
+        self.hp = 50
+        self.at = 20
+        self.inv = []
+        self.dodge = 12
+
+
+class Ciberenomy:
+    """docstring for ClassName"""
+    def __init__(self):
+        self.name = "Киберзлодей"
+        self.hp = 100
+        self.at = 5
+        self.inv = []
+        self.dodge = 10
+
+
+
+pl1 = Ciberhero()
+print("Приветствую тебя Кибербогатырь ты попал в свое первое путешествие, хочешь жить и продолжить путешествие ? Тогда принеси мне деньги их ты можешь найти на своей дороге ")
+print("Что вы выберете? 1 - Соглоситься и пойти искать монеты, 2 - Отказаться и пытаться убежать от царя")
+kay = input()
+if kay == "1":
+    print("Кибербогатырь согласился и пошел по дороге к своим путешествим, зная что его ждет куча преград")
+
+elif kay == "2":
+    print("- Я тебя предупреждал,ответил царь и убил вашего кибербогатыря, Вы открыли плохую концовку")
+else:
+    print("- Кибер баготырь свахатил меч и убил царя и зажил славной жизнью, Вы открыли секретную концовку")
+
+
+
+
+
+
 
